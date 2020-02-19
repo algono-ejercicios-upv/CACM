51b14c6ff168475efa50f2cf4d668485d66ff478
Alejandro Gómez <algono@inf.upv.es>
2020-02-19 21:18:37 +0100

    Steps problem finished

--- Steps/Steps.py
+++ Steps/Steps.py
@@ -12,26 +12,17 @@ def steps(x, y):
             total = int(top_int**2)
             top = top_int
             left = end - total
-            if left > top:
-                min_times = left // top
-                if min_times < left:
-                    """
-                    TODO: Fix this trying the other cases mentioned.
-
-                    Explanation:
-
-                    When end = 23, the extra (min steps) is (3, 3, 1)
-                    We are only trying combinations of the same number
-                    (i.e.: (1, 1, 1), (2, 2, 2), etc.)
-                    But not cases like this.
-                    """
-                    for times in range(min_times, left):
-                        num = left / times
-                        if num % 1 == 0 and num < top:
-                            return count + times
-                return count + left  # 1s left times
-            else:
+            while top > 1 and left > top:
+                times = left // top
+                count += times
+                extra = top * times
+                total += extra
+                left -= extra
+                top -= 1
+            if top > 1 and left > 0:
                 return count + 1  # left 1 time
+            else:
+                return count + left  # 1s left times
 
 
 test_cases = int(input())
