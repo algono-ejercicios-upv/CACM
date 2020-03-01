d1f2d84e0e38fe7e720e6ee6922828898158dedd
Alejandro Gómez <algono@inf.upv.es>
2020-03-01 11:51:14 +0100

    Some work on doublets problem

--- Programming-Challenges/Chapters/3/Doublets/doublets.py
+++ Programming-Challenges/Chapters/3/Doublets/doublets.py
@@ -14,10 +14,11 @@ def str_diff_closure(one: str):
 
 
 def str_inc(ref: str, before: str, after: str):
-    return str_diff(after, ref) - str_diff(before, ref)
+    actual_inc = str_diff(after, ref) - str_diff(before, ref)
+    return actual_inc if actual_inc > 0 else 0
 
 
-def path_len(diff: int, inc: int):
+def route_len(diff: int, inc: int):
     return 2*inc + diff
 
 
@@ -35,58 +36,103 @@ def are_doublets(one: str, two: str):
         return False
 
 
-def doublet_route(words: list, start: str, end: str):
+def check_base(words: list, start: str, end: str, **kwargs):
+    """
+    kwargs:
+    - max_len: max length of route to be considered valid
+    - func: function called after checking the base (args passed: words, start, end)
+    """
+    if len(words) == 0:
+        return None
+    elif start == end:
+        return ([start], 0)
+    else:
+        diff = str_diff(start, end)
+        if diff:
+            max_len = kwargs['max_len']
+            if max_len:
+                if diff >= max_len:
+                    return None
+                elif diff == max_len - 2:
+                    return ([start, end], 0) if are_doublets(start, end) else None
+
+            func = kwargs['func']
+            return func(words, start, end) if func else None
+        else:
+            return None  # Start and end must have the same length
+
+
+def doublet_route_dfs(words: list, start: str, end: str, max_len=None):
     """
     Uses DFS to find the perfect minimal route.
     If no perfect route is found, it switches to BFS to find the minimal.
 
-    Returns: Minimal doublet route between start and end
+    Returns: Tuple (route, inc) minimal
     """
-    if start == end:
-        return [start]
-    else:
-        diff = str_diff(start, end)
-        if diff:
-            doublets = [w for w in words if are_doublets(w, start)]
-            min_path_word = min(doublets, key=str_diff_closure(end))
-            next_diff = str_diff(min_path_word, end)
+    return check_base(words, start, end, max_len=max_len, func=doublet_route_dfs_impl)
 
-            if diff > next_diff:
-                return [start] + doublet_route(words, min_path_word, end)
+
+def doublet_route_dfs_impl(words: list, start: str, end: str):
+    doublets = [w for w in words if are_doublets(w, start)]
+    min_path_word = min(doublets, key=str_diff_closure(end))
+    inc = str_inc(end, start, min_path_word)
+    if inc == 0:
+        res = doublet_route_dfs([w for w in words if w != start], min_path_word, end)
+        if res:
+            next_route, next_inc = res
+            route = [start] + next_route
+            inc += next_inc
+            if inc == 0:
+                return (route, inc)
             else:
-                return [start] + doublet_route_bfs(words, min_path_word, end)
+                min_res = (route, inc)
+                min_len = route_len(str_diff(start, end), inc)
+                res = doublet_route_dfs([w for w in words if w != start], min_path_word, end, max_len=min_len)
+                
+                while res:
+                    next_route, next_inc = res
+                    route = [start] + next_route
+                    inc += next_inc
+                    min_res = (route, inc)
+                    min_len = route_len(str_diff(start, end), inc)
+                    res = doublet_route_dfs([w for w in words if w != start], min_path_word, end, max_len=min_len)
+                
+                return min_res
         else:
-            # Start and end must have the same length
-            return None
+            return res
+    else:
+        return doublet_route_bfs(words, start, end)
 
 
-def doublet_route_bfs(words: list, start: str, end: str):
+def doublet_route_bfs(words: list, start: str, end: str, max_len=None):
     """
     Uses BFS to find the minimal route.
 
     Returns: Minimal doublet route between start and end
     """
-    if start == end:
-        return [start]
-    else:
-        next_routes = [[start]]
-        next_words = [[]]
+    return check_base(words, start, end, max_len=max_len, func=doublet_route_bfs_impl)
 
-        remaining_words = [w for w in words if w != start]
 
-        for word in words:
-            if are_doublets(start, word):
-                if word == end:
-                    return [start, end]
-                else:
-                    next_words[0].append(word)
+def doublet_route_bfs_impl(words: list, start: str, end: str):
+    next_routes = [[start]]
+    next_words = [[]]
 
-        while len(remaining_words) > 0 and len(next_routes) > 0:
-            routes, current_words = next_routes, next_words
-            next_routes, next_words = list(), list()
+    remaining_words = [w for w in words if w != start]
 
-            for index, route in enumerate(routes):
-                for word in current_words[index]:
+    for word in words:
+        if are_doublets(start, word):
+            if word == end:
+                return [start, end]
+            else:
+                next_words[0].append(word)
+
+    while len(remaining_words) > 0 and len(next_routes) > 0:
+        routes, current_words = next_routes, next_words
+        next_routes, next_words = list(), list()
+
+        for index, route in enumerate(routes):
+            for word in current_words[index]:
+                if word in remaining_words:
                     if word == end:
                         route.append(word)
                         return route
@@ -102,8 +148,8 @@ def doublet_route_bfs(words: list, start: str, end: str):
                         if len(word_doublets) > 0:
                             next_routes.append(word_route)
                             next_words.append(word_doublets)
-        else:
-            return None
+    else:
+        return None
 
 
 try:
@@ -122,8 +168,13 @@ try:
             started = True
 
         word_one, _, word_two = pair.partition(' ')
-        route = doublet_route(words, word_one, word_two)
-        print(*route, sep='\n') if route else print('No solution')
+        res = doublet_route_dfs(words, word_one, word_two)
+        if res:
+            route, inc = res
+            print(*route, sep='\n')
+        else:
+            print('No solution')
+        
         pair = input()
 except EOFError as err:
     pass
