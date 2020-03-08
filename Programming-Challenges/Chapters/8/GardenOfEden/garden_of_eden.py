77e88a68d73621c2066b01df5d1c9acb9df1ad5e
Alejandro Gómez <algono@inf.upv.es>
2020-03-08 19:50:30 +0100

    Some optimizations (garden of eden)

--- Programming-Challenges/Chapters/8/GardenOfEden/garden_of_eden.py
+++ Programming-Challenges/Chapters/8/GardenOfEden/garden_of_eden.py
@@ -1,18 +1,15 @@
 N_RULES = 8
 
 
-def is_reachable(n, rules, cell_pos, origin, target):
+def is_reachable(cell_pos, curr_state):
     if cell_pos == n-1:
-        s_last = (origin[n-2] << 2) | (origin[n-1] << 1) | (origin[0] << 0)
-        s_first = (origin[n-1] << 2) | (origin[0] << 1) | (origin[1] << 0)
-        return target[cell_pos] == rules[s_last] and target[0] == rules[s_first]
+        return ((curr_state >> 1) & 1) == ((first >> 2) & 1) and (curr_state & 1) == ((first >> 1) & 1)
     else:
+        visited.add((cell_pos, curr_state))
         for i in range(N_RULES):
-            if rules[i] == target[cell_pos] and ((i >> 2) & 1) == origin[cell_pos-1] and ((i >> 1) & 1) == origin[cell_pos]:
-                res = is_reachable(n, rules,
-                                   cell_pos+1, origin + [(i >> 0) & 1], target)
-                if res:
-                    return res
+            if ((a_id >> i) & 1) == int(a_state[cell_pos]) and ((curr_state >> 1) & 1) == ((i >> 2) & 1) and (curr_state & 1) == ((i >> 1) & 1):
+                if (cell_pos+1, curr_state) not in visited and is_reachable(cell_pos+1, i):
+                    return True
         else:
             return False
 
@@ -25,16 +22,11 @@ try:
         a_id, n, a_state = int(a_params[0]), int(
             a_params[1]), a_params[2]
 
-        target = [int(s) for s in a_state]
-
-        rules = [(a_id >> i) & 1 for i in range(N_RULES)]
-
         reachable = False
         for i in range(N_RULES):
-            if rules[i] == target[0]:
-                origin = [(i >> 1) & 1, (i >> 0) & 1]
-
-                reachable = is_reachable(n, rules, 1, origin, target)
+            if ((a_id >> i) & 1) == int(a_state[0]):
+                visited, first = set(), i
+                reachable = is_reachable(0, i)
 
                 if reachable:
                     break
