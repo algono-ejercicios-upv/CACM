ab9b144c4b6b88137658d148d2923f2ce233a50c
Alejandro Gómez <algono@inf.upv.es>
2020-03-08 16:23:03 +0100

    Garden of Eden new approach: Bitwise operators

--- Programming-Challenges/Chapters/8/GardenOfEden/garden_of_eden.py
+++ Programming-Challenges/Chapters/8/GardenOfEden/garden_of_eden.py
@@ -1,60 +1,46 @@
-def is_reachable(automaton_state, number_of_cells, evolution_rules):
-    return is_reachable_impl(automaton_state, number_of_cells, evolution_rules, 0, None, None)
+N_RULES = 8
 
 
-def is_reachable_impl(automaton_state, number_of_cells, evolution_rules, cell_pos, first_full_cell_state, last_full_cell_state):
-    if cell_pos == number_of_cells:
-        return first_full_cell_state[:2] == last_full_cell_state[-2:]
+def is_reachable(n, rules, cell_pos, origin, target):
+    if cell_pos == n-1:
+        s_last = (origin[n-2] << 2) | (origin[n-1] << 1) | (origin[0] << 0)
+        s_first = (origin[n-1] << 2) | (origin[0] << 1) | (origin[1] << 0)
+        return target[cell_pos] == rules[s_last] and target[0] == rules[s_first]
     else:
-        cell_state = automaton_state[cell_pos]
-
-        for full_cell_state in evolution_rules[cell_state]:
-            # It is only valid if the first two digits of it match the last one's last two
-            if last_full_cell_state == None or last_full_cell_state[-2:] == full_cell_state[:2]:
-                res = is_reachable_impl(automaton_state, number_of_cells, evolution_rules, cell_pos+1,
-                                        full_cell_state if first_full_cell_state == None else first_full_cell_state, full_cell_state)
+        for i in range(N_RULES):
+            if rules[i] == target[cell_pos] and ((i >> 2) & 1) == origin[cell_pos-1] and ((i >> 1) & 1) == origin[cell_pos]:
+                res = is_reachable(n, rules,
+                                   cell_pos+1, origin + [(i >> 0) & 1], target)
                 if res:
                     return res
         else:
             return False
 
 
-# Every possible rule input (L-C-R) (0-0-0 -> 1-1-1)
-rule_inputs = [
-    '000',
-    '001',
-    '010',
-    '011',
-    '100',
-    '101',
-    '110',
-    '111'
-]
-
-
 try:
-    automaton_str = input()
-    while automaton_str:
-        automaton_params = automaton_str.split(' ')
+    a_str = input()
+    while a_str:
+        a_params = a_str.split(' ')
 
-        automaton_id, number_of_cells, automaton_state = int(automaton_params[0]), int(
-            automaton_params[1]), automaton_params[2]
+        a_id, n, a_state = int(a_params[0]), int(
+            a_params[1]), a_params[2]
 
-        evolution_rules = {'0': [], '1': []}  # All possible states for a cell
+        target = [int(s) for s in a_state]
 
-        automaton_id_left = automaton_id
-        # For every possible rule input
-        for rule in rule_inputs:
-            # Calculate the state it generates (from the automaton id)
-            out_state = str(automaton_id_left % 2)
-            # Take out the state from the id
-            automaton_id_left //= 2
-            # Store into a list of which rules generate that state
-            evolution_rules[out_state].append(rule)
+        rules = [(a_id >> i) & 1 for i in range(N_RULES)]
 
-        print('REACHABLE' if is_reachable(automaton_state, number_of_cells,
-                                          evolution_rules) else 'GARDEN OF EDEN')
+        reachable = False
+        for i in range(N_RULES):
+            if rules[i] == target[0]:
+                origin = [(i >> 1) & 1, (i >> 0) & 1]
 
-        automaton_str = input()
+                reachable = is_reachable(n, rules, 1, origin, target)
+
+                if reachable:
+                    break
+
+        print('REACHABLE' if reachable else 'GARDEN OF EDEN')
+
+        a_str = input()
 except EOFError as err:
     pass
