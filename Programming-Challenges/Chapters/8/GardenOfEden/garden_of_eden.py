a4ce7afd7cafbae3840f7d22d75f253afc4ed0ec
Alejandro Gómez <algono@inf.upv.es>
2020-03-09 11:44:34 +0100

    Some improvement (Garden of eden) By using bytearray instead of set (for visited)

--- Programming-Challenges/Chapters/8/GardenOfEden/garden_of_eden.py
+++ Programming-Challenges/Chapters/8/GardenOfEden/garden_of_eden.py
@@ -5,10 +5,10 @@ def is_reachable(cell_pos, curr_state):
     if cell_pos == n-1:
         return ((curr_state >> 1) & 1) == ((first >> 2) & 1) and (curr_state & 1) == ((first >> 1) & 1)
     else:
-        visited.add((cell_pos, curr_state))
+        visited[cell_pos*curr_state] = True
         for i in range(N_RULES):
             if ((a_id >> i) & 1) == int(a_state[cell_pos]) and ((curr_state >> 1) & 1) == ((i >> 2) & 1) and (curr_state & 1) == ((i >> 1) & 1):
-                if (cell_pos+1, curr_state) not in visited and is_reachable(cell_pos+1, i):
+                if not visited[cell_pos+1 * curr_state] and is_reachable(cell_pos+1, i):
                     return True
         else:
             return False
@@ -25,7 +25,7 @@ try:
         reachable = False
         for i in range(N_RULES):
             if ((a_id >> i) & 1) == int(a_state[0]):
-                visited, first = set(), i
+                visited, first = bytearray(n * N_RULES), i
                 reachable = is_reachable(0, i)
 
                 if reachable:
