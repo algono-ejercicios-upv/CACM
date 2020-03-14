908ce04c306fec8479a40e5028d5837799388f27
Alejandro Gómez <algono@inf.upv.es>
2020-03-14 18:26:06 +0100

    Working solution, but TLE (ShellSort)

--- Programming-Challenges/Chapters/4/ShellSort/shellsort.cpp
+++ Programming-Challenges/Chapters/4/ShellSort/shellsort.cpp
@@ -1,12 +1,60 @@
-#include <string>   // for string datatype
-#include <iostream> // for cin / cout (standard input/output)
-#include <cstring>  // for the memset function
+#include <string>
+#include <vector>
+#include <iostream>      // for cin / cout (standard input/output)
+#include <unordered_map> // hash table (access O(1))
+#include <algorithm>     // sort
 
 using namespace std;
 
 #define MAX_TURTLES 200
 
 int numberOfTurtles;
+
+string original[MAX_TURTLES];
+unordered_map<string, int> required;
+
+int dist[MAX_TURTLES];
+
+vector<int> results;
+
+vector<int> shellSort()
+{
+    results = {};
+    for (int i = 0; i < numberOfTurtles; i++)
+    {
+        dist[i] = required[original[i]] - i;
+        if (dist[i] < 0)
+        {
+            results.push_back(i);
+            int reduction = 1;
+            for (int j = i - 1; j >= 0; j--)
+            {
+                dist[j] -= reduction;
+                if (dist[j] < 0)
+                {
+                    results.push_back(j);
+                    reduction++;
+                }
+            }
+        }
+    }
+    return results;
+}
+
+int greaterRequiredPos(int i, int j)
+{
+    return required[original[i]] > required[original[j]];
+}
+
+void printResults(vector<int> results)
+{
+    sort(results.begin(), results.end(), greaterRequiredPos);
+
+    for (int resultIndex : results)
+    {
+        cout << original[resultIndex] << '\n';
+    }
+}
 
 int main()
 {
@@ -19,13 +67,12 @@ int main()
     int testCases;
     cin >> testCases;
 
+    bool notFirst = false;
     for (int i = 0; i < testCases; i++)
     {
         cin >> numberOfTurtles;
 
         cin.ignore(); // Ignore the rest of the line after the number of turtles
-
-        string original[MAX_TURTLES], required[MAX_TURTLES];
 
         string turtle;
         for (int j = 0; j < numberOfTurtles && getline(cin, turtle); j++)
@@ -34,7 +81,18 @@ int main()
         }
         for (int j = 0; j < numberOfTurtles && getline(cin, turtle); j++)
         {
-            required[j] = turtle;
+            required[turtle] = j;
         }
+
+        if (notFirst)
+        {
+            cout << '\n';
+        } // Print a new line between cases
+        else
+        {
+            notFirst = true;
+        }
+
+        printResults(shellSort());
     }
 }
